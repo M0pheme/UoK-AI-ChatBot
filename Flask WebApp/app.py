cb538a2f47f9d6b9a0bad682d097dcb1c744188f
M0pheme <willfredwigans@gmail.com>
2025-08-28 21:03:44 +0200

    Flask WebApp for ChatBot

---
+++ Flask WebApp/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify, render_template
+
+from utils import get_response, predict_class
+
+app = Flask(__name__, template_folder='templates')
+
+
+# Home page of the website
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Login page
+'''@app.route('/login', methods=['POST'])
+def login():
+    return render_template('login.html')'''
+
+# Registration page
+'''@app.route('/register', methods=['POST'])
+def register():
+    return render_template('register.html')'''
+
+
+@app.route('/handle_message', methods=['POST'])
+def handle_message():
+    message = request.json['message']
+    intents_list = predict_class(message)
+    response = get_response(intents_list)
+
+    return jsonify({'response': response})
+
+
+# curl -X POST http://127.0.0.1:5000/handle_message -d '{"message":"what is coding"}' -H "Content-Type: application/json"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
